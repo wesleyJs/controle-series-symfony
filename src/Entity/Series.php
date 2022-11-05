d9b187a0aecc0d3711234d4813f3a27782b56e4a
wesley viana martins <wesley.vmartins.js@gmail.com>
2022-11-05 18:18:56 -0400

    criado relacionamento de Series, seasons e episodes

--- src/Entity/Series.php
+++ src/Entity/Series.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SeriesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -14,15 +16,19 @@ class Series
     #[ORM\Column]
     private int $id;
 
+    #[ORM\OneToMany(mappedBy: 'series', targetEntity: Season::class, orphanRemoval: true)]
+    private Collection $seasons;
+
     public function __construct(
         #[ORM\Column]
         #[Assert\NotBlank]
         #[Assert\Length(min: 3)]
         private string $name = ''
     ) {
+        $this->seasons = new ArrayCollection();
     }
 
-    public function getId(): ?int
+    public function getId(): int
     {
         return $this->id;
     }
@@ -35,6 +41,36 @@ class Series
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Season>
+     */
+    public function getSeasons(): Collection
+    {
+        return $this->seasons;
+    }
+
+    public function addSeason(Season $season): self
+    {
+        if (!$this->seasons->contains($season)) {
+            $this->seasons->add($season);
+            $season->setSeries($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSeason(Season $season): self
+    {
+        if ($this->seasons->removeElement($season)) {
+            // set the owning side to null (unless already changed)
+            if ($season->getSeries() === $this) {
+                $season->setSeries(null);
+            }
+        }
 
         return $this;
     }
